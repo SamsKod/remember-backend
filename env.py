2851ec355282633200da78596338185a0b17104c
samuelwigren <samuel.wigren@gsfacket.se>
2023-07-16 17:24:34 +0200

    Remove env.py and dbsqlite3 from Github-repo

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['CLOUDINARY_URL'] = 'cloudinary://981358421114349:CYozJwU17uLnT5l-PhOfx6ORx8k@due4uu8h3'
-os.environ['DEV'] = '1'
-os.environ['DATABASE_URL'] = "postgres://afenqeda:gVOkFNTNDQngumbgmnzg87JN1ORjMg4H@snuffleupagus.db.elephantsql.com/afenqeda"
-os.environ.setdefault("SECRET_KEY", "MySecretKeyValue4remember")
